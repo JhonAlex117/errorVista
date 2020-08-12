859edb0b94ca900004bcd9ff4d894f8158f6591f
JhonAlex117 <jhonalexsalazar04@gmail.com>
2020-08-11 23:33:02 -0500

    primer commit

---
+++ src/routes/coustomers.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const coustomerController = require('../controllers/coustomercontrollers');
+
+router.get('/user', coustomerController.list);
+router.post('/add', coustomerController.save);
+router.get('/delete/:id', coustomerController.delete);
+
+router.get('/update/:id', coustomerController.edit);
+router.post('/update/:id', coustomerController.update);
+
+
+router.get('/produc', coustomerController.listproduct);
+
+
+
+
+
+module.exports = router;
